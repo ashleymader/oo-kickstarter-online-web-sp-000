b5431241155bb2b8f3fad104a3545179e2a29565
Ashley M Mader <ashley.mader12@gmail.com>
2020-03-17 19:42:55 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -3,9 +3,14 @@ require 'pry'
 class Project 
   
   attr_accessor :title, :backers
+  
   def initialize(title)
     @title = title
     @backers = []
+  end
+  
+  def add_backer(backer)
+    @backers << backer 
   end
   
   
